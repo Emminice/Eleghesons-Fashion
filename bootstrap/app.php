661fbe164f0b151fa2e96bae4f309642dd6ee820
Emminice <158488812+Emminice@users.noreply.github.com>
2026-06-06 22:48:51 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => AdminMiddleware::class,
             'customer' => CustomerMiddleware::class,
         ]);
+
+        $middleware->trustProxies(
+            at: '*'
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
